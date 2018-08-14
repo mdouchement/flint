428a7c2ab5fea3bdc47a96d0d6854477de435731
z0mbie42 <sylvain.kerkour@gmail.com>
2018-08-14 11:13:36 +0200

    dependencies: add dep files

--- lint/config.go
+++ lint/config.go
@@ -5,8 +5,8 @@ type Arguments = []interface{}
 
 // RuleConfig is type used for the rule configuration.
 type RuleConfig struct {
-	Arguments Arguments `toml:"arguments,omitempty"`
-	Severity  Severity  `toml:"severity,omitempty"`
+	Arguments Arguments `toml:"arguments,omitempty" json:"arguments,omitempty"`
+	Severity  Severity  `toml:"severity,omitempty" json:"severity,omitempty"`
 }
 
 // RulesConfig defines the config for all rules.
@@ -26,13 +26,13 @@ type DefinedRules = map[string]DefinedRule
 type Config struct {
 	// the directory of the config file, relative to the execution of flint
 	// Extends []strings // extend a set of rules
-	BasePath    string      `toml:"-"`
-	Description string      `toml:"description"`
-	Format      string      `toml:"format"`
-	Severity    Severity    `toml:"severity"`
-	Rules       RulesConfig `toml:"rules"`
-	ErrorCode   int         `toml:"error_code"`
-	WarningCode int         `toml:"warning_code"`
+	// for json which does not have comments
+	Comment     string      `toml:"description" json:"description"`
+	Format      string      `toml:"format" json:"format"` // output format
+	Severity    Severity    `toml:"severity" json:"severity"`
+	Rules       RulesConfig `toml:"rules" json:"rules"`
+	ErrorCode   int         `toml:"error_code" json:"error_code"`
+	WarningCode int         `toml:"warning_code" json:"warning_code"`
 	//IgnoredFiles       []string     `toml:"ignored_files"`
 	//IgnoredDirectories []string     `toml:"ignored_directories"`
 	//DefinedRules DefinedRules `toml:"defined_rules"`
